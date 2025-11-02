574a4dd136df2029456b65e0d6c0c0066802b324
jramosss <jramostod@gmail.com>
2025-11-02 16:05:22 -0300

    :broom:

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 import base64
 import sqlite3
-from time import sleep
 
 from playwright.async_api import async_playwright
 
--- printer.py
+++ printer.py
@@ -3,14 +3,19 @@ import sqlite3
 conn = sqlite3.connect("cuestionarios.db")
 c = conn.cursor()
 
-query = "SELECT * FROM preguntas LIMIT 10"
+query = "SELECT * FROM preguntas LIMIT 250"
 results = c.execute(query)
+text = ""
 
-for row in results:
+for row in list(results):
     pregunta = row[1]
-    print(pregunta)
+    text += f"{pregunta}\n"
     respuestas = c.execute("SELECT * FROM respuestas WHERE pregunta_id = ?", (row[0],))
     for respuesta in respuestas:
-        print(respuesta[2], "---" "Correcta" if respuesta[3] == 1 else "")
+        text += f"{respuesta[2]} {'---Correcta' if respuesta[3] == 1 else ''}\n"
+    text += "\n"
+
+with open("cuestionarios.txt", "w") as f:
+    f.write(text)
 
 conn.close()
